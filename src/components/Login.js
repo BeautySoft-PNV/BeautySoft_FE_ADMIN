b9f0938631dd4c67d38e5975a219d1b2485118fb
Your Name <you@example.com>
2025-03-24 23:44:29 +0700

    deyloy

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
             return;
         }
         try {
-            const response = await fetch("http://18.142.0.155:5001/api/auth/login", {
+            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/auth/login`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
